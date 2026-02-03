57ff1fe9c37d9867ab2ee94c686ca874c2f1350a
SikHye1977 <sikhye1977@naver.com>
2026-02-03 18:27:58 +0900

    chore: remove useless comment

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -1,4 +1,2 @@
-// src/types/declarations.d.ts (또는 루트 경로의 declarations.d.ts)
-
 declare module 'reedsolomon';
-declare module 'react-native-get-random-values'; // 혹시 얘도 에러나면 추가
+declare module 'react-native-get-random-values';
